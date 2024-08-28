65b2c0339cd2f9223e305de78b9ac9885d908f77
ACJT123 <acjtacjt@gmail.com>
2024-08-28 11:37:40 +0800

    remove

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { compression } from "vite-plugin-compression2";
-import { analyzer } from "vite-bundle-analyzer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), compression(), analyzer()],
+  plugins: [react(), compression()],
 });
